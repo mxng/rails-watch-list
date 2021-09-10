9cf679c23409ff977040034de8267baa4ed72901
Min Xuan Ng <hi@ngminxuan.com>
2021-09-10 15:37:25 +0800

    added movies controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,8 +1,20 @@
 class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
   def new
+    @bookmark = Bookmark.new
   end
 
   def create
+    @bookmark = Bookmark.new(bookmark_params)
+
+    if @bookmark.save
+      redirect_to bookmarks_path
+    else
+      render :new
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/create'
-  get 'bookmarks/destroy'
+  get 'movies/index'
+  get 'movies/show'
+  get 'movies/destroy'
+  # get 'bookmarks/new'
+  # get 'bookmarks/create'
+  # get 'bookmarks/destroy'
   # get 'lists/index'
   # get 'lists/show'
   # get 'lists/new'
   # get 'lists/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index, :show, :new, :create]
+  resources :lists, only: %i[index show new create]
+  resources :bookmarks, only: %i[index new create destroy]
 end
