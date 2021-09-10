699f1732813973b2dec84d72b9f1a8640730c3d9
Min Xuan Ng <hi@ngminxuan.com>
2021-09-10 16:24:22 +0800

    working form for new bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,14 +6,18 @@ class BookmarksController < ApplicationController
   def new
     @bookmark = Bookmark.new
     @list = List.find(params[:list_id])
+    # @movie = Movie.find(params["bookmark"][:movie_id])
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
+    @movie = Movie.find(params["bookmark"][:movie_id])
+    @bookmark.movie = @movie
+    @bookmark.list = @list
 
     if @bookmark.save
-      redirect_to list_bookmarks_path
+      redirect_to list_path(@list)
     else
       render :new
     end
@@ -27,4 +31,8 @@ class BookmarksController < ApplicationController
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie, :list)
   end
+
+  # def movie_params
+  #   params.require(:movie).permit(:title)
+  # end
 end
