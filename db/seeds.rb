75220d66de9641b12da099364cd29d19b0a84fd6
Min Xuan Ng <hi@ngminxuan.com>
2021-09-10 14:02:19 +0800

    added routes and seeded movie data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+
+puts "creating movies"
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+raw = URI.open(url).read
+serialized = JSON.parse(raw)
+
+10.times do |index|
+  movie = Movie.new(
+    title: serialized["results"][index]["title"],
+    overview: serialized["results"][index]["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original#{serialized['results'][index]['poster_path']}",
+    rating: serialized["results"][index]["vote_average"]
+  )
+  movie.save!
+  puts "Created #{serialized['results'][index]['title']}"
+end
+
+puts "completed"
